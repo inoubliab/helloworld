d8db02dc7e0f0b4ce10de5123c6dd769cc415351
liushuai <249359802@qq.com>
2012-09-20 16:21:31 +0800

    add method print()

--- src/test/helloworld.java
+++ src/test/helloworld.java
@@ -8,6 +8,10 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		print();
+	}
+	private static void print(){
+		System.out.println("second");
 	}
 
 }
